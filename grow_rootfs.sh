a76b5b5da4d995addf3010e02bb2666a19643160
Nont Banditwong <nontster@gmail.com>
2018-10-16 05:38:14 +0700

    first commit

---
+++ grow_rootfs.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+function osrelease () {
+    OS=`cat /etc/os-release | grep '^NAME=' |  tr -d \" | sed 's/\n//g' | sed 's/NAME=//g'`
+    
+    if [ "$OS" == "Ubuntu" ]; then
+        echo "Ubuntu"
+    elif [ "$OS" == "Amazon Linux AMI" ]; then
+        echo "AMZN"
+    elif [ "$OS" == "CentOS Linux" ]; then
+        echo "CentOS"
+    else
+        echo "Operating System Not Found"
+    fi
+}
+
+function vgname () {
+    echo `vgs --noheadings -o vg_name`
+}
+
+vg=$(vgname)
+release=$(osrelease)
+
+if [ "$release" == "CentOS" ]; then
+	echo -e "n\np\n\n\n\nt\n\n8e\nw" | fdisk /dev/sda
+    partprobe
+	pvcreate /dev/sda3
+	vgextend $vg /dev/sda3
+	lvextend -l +100%FREE /dev/$vg/root
+	xfs_growfs /dev/$vg/root
+elif [ "$release" == "Ubuntu" ]; then
+	echo -e "n\np\n\n\nt\n\n8e\nw" | fdisk /dev/sda
+    partprobe
+	pvcreate /dev/sda4
+	vgextend $vg /dev/sda4
+	lvextend -l +100%FREE /dev/$vg/root
+	resize2fs /dev/$vg/root
+fi
